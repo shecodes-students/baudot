3f9e2f918cfbd04ce01f4544a379cd64068a70e3
LenaKari <olena.levitsky@gmail.com>
2014-09-29 15:34:56 +0100

    Updated calculation to include inside points

--- calc.js
+++ calc.js
@@ -1,14 +1,25 @@
 var cx = 300;
 var cy = 200;
-var r = 140;
+var r = 150;
 
 var alpha = -3.5 * Math.PI / 15;
-var x = cx + r * Math.cos(alpha);
-var y = cy + r * Math.sin(alpha);
+var x = (cx + r * Math.cos(alpha)).toFixed(2);
+var y = (cy + r * Math.sin(alpha)).toFixed(2);
 
 var sx = cx;
 var sy = cy - r;
 
+console.log('M' + sx + ',' + sy + ' A' + r + ',' + r + ' 0 0,1 ' + x + ',' + y );
 
-console.log('M' + sx + ', ' + sy + ' a' + r + ', ' + r + ' 0 0,1 ' + x + ', ' + y );
 
+
+//First inside point
+var innerRadius = r - 15;
+var firstInnerX = (cx + innerRadius * Math.cos(alpha)).toFixed(2);
+var firstInnerY = (cy + innerRadius * Math.sin(alpha)).toFixed(2);
+
+console.log('L' + firstInnerX + ',' + firstInnerY);
+
+
+//Second inside point
+console.log('A' + innerRadius + ',' + innerRadius + ' 0 0,0 ' + cx + ',' + (cy-innerRadius));
