a065629ed48ad6ea6f1d22a155cae8991ae4999d
Jan Bölsche <regular.gonzales@googlemail.com>
2014-10-02 21:05:21 +0200

    using ejs template for svg, corrected segment placements, added hand, added hand animation and keyboard handler, highlighing sections when associated key is pressed

--- calc.js
+++ calc.js
@@ -1,25 +1,34 @@
-var cx = 300;
-var cy = 200;
 var r = 150;
 
 var alpha = -3.5 * Math.PI / 15;
-var x = (cx + r * Math.cos(alpha)).toFixed(2);
-var y = (cy + r * Math.sin(alpha)).toFixed(2);
+var x = (r * Math.cos(alpha)).toFixed(2);
+var y = (r * Math.sin(alpha)).toFixed(2);
 
-var sx = cx;
-var sy = cy - r;
+var sx = 0;
+var sy = -r;
 
-console.log('M' + sx + ',' + sy + ' A' + r + ',' + r + ' 0 0,1 ' + x + ',' + y );
+module.exports.makeSegmentPath = function() {
+    var result = "";
+
+    result += 'M' + sx + ',' + sy + ' A' + r + ',' + r + ' 0 0,1 ' + x + ',' + y + ' ';  
+
+    //First inside point
+    var innerRadius = r - 15;
+    var firstInnerX = (innerRadius * Math.cos(alpha)).toFixed(2);
+    var firstInnerY = (innerRadius * Math.sin(alpha)).toFixed(2);
+
+    result += 'L' + firstInnerX + ',' + firstInnerY + ' ';
+
+
+    //Second inside point
+    result += 'A' + innerRadius + ',' + innerRadius + ' 0 0,0 0' + (-innerRadius) + ' z';
+
+    return result;
+};
+
+module.exports.getSegmentRotation = function(segmentIndex) { 
+    return 'rotate (' + (segmentIndex * 360 / 7.5).toFixed(2) + ' 0 0)';
+};
 
 
 
-//First inside point
-var innerRadius = r - 15;
-var firstInnerX = (cx + innerRadius * Math.cos(alpha)).toFixed(2);
-var firstInnerY = (cy + innerRadius * Math.sin(alpha)).toFixed(2);
-
-console.log('L' + firstInnerX + ',' + firstInnerY);
-
-
-//Second inside point
-console.log('A' + innerRadius + ',' + innerRadius + ' 0 0,0 ' + cx + ',' + (cy-innerRadius));
--- index.js
+++ index.js
@@ -1,11 +1,47 @@
 var test = require('prova');
 var fragment = require('fragment');
 var fs = require('fs');
+var svgTemplate = require('./baudot.ejs');
+var calc = require('./calc');
 
 test('display', function(assert) {
     assert.plan(1);
-    var svg = fs.readFileSync("./test.svg", "utf8");    
+    var svg = svgTemplate(calc);
     var frag = fragment(svg);
     document.body.insertBefore(frag, document.body.childNodes[0]);
+    var hand = document.getElementById('hand');
+    
+    var angle = 0;
+    var baudrate = 0.75; // bits per seconds
+    var bitsPerRotation = 7.5;
+    var startTime = new Date().getTime();
+    function update() {
+        var currentTime = new Date().getTime();
+        var t = currentTime - startTime;
+        angle = 360 * baudrate / bitsPerRotation * t / 1000;
+        hand.setAttribute('transform', "rotate(" + angle + ")");
+    }
+    function segmentFromKeyCode(keyCode) {
+        var segment = keyCode - 49;
+        if (segment >= 0 && segment < 5) {
+            return segment + 1; // because segment 0 is the start bit
+        }
+        return undefined;
+    }
+    document.onkeydown = createKeyHandler('white'); 
+    document.onkeyup = createKeyHandler('none'); 
+    
+    function createKeyHandler(strokeColor) {    
+        return function (e) {
+            e = e || window.event;
+            var segmentIndex = segmentFromKeyCode(e.keyCode);
+            if (typeof segmentIndex === 'undefined') return;
+            var segment = document.getElementById('segment_' + segmentIndex);
+            segment.setAttribute("stroke", strokeColor);
+        };
+    }
+
+
+    window.setInterval(update, 1000/60);
     assert.ok(true);
 });
